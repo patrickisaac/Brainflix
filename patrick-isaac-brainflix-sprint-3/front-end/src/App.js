be4068b8514716645dcbc3c0f33d69bd41b000ad
Patrick Isaac <patrick.isaac04@gmail.com>
2019-02-28 21:44:11 -0800

    folder structure for sprint 3

---
+++ patrick-isaac-brainflix-sprint-3/front-end/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Home from './Home'
+import Nav from './components/Nav'
+import { Route, Switch } from 'react-router-dom'
+import UploadPage from './components/UploadPage'
+import './App.css'
+// import Axios from 'axios';
+
+//USED FOR NAVIGATION COMP.
+const profile = [
+  "/Assets/Images/Mohan-muruge.jpg"
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Nav profile={profile}/>
+        <Switch>
+          <Route path="/" exact render={(routeProps) => {
+            return <Home {...routeProps} />
+          }} />
+          <Route path="/upload" component={UploadPage} />
+          <Route path="/videos/:id" render={(routeProps) => {
+            return <Home {...routeProps} />
+          }} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ patrick-isaac-brainflix-sprint-3/front-end/src/components/VideoInfo.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import "../global_css/videoInfo_css/styles.css"
+
+const date = (d) => {
+  return new Date(d).toLocaleDateString()
+}
+
+class VideoInfo extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="video__title">{this.props.mainVideo.title}</h1>
+      <div className="video__container">
+        <div className="video__info">
+          <h4 className="video__channel">By {this.props.mainVideo.channel}</h4>
+          <p className="video__date">{date(this.props.mainVideo.timestamp)}</p>
+        </div>
+        <div className="video__stats">
+          <div className="video__views">
+          <img src="/Assets/Icons/SVG/Icon-views.svg" alt="views"/>
+          <p>{this.props.mainVideo.views}</p>
+          </div>
+          <div className="video__likes">
+          <img src="/Assets/Icons/SVG/Icon-likes.svg" alt="likes"/>
+          <p>{this.props.mainVideo.likes}</p>
+          </div> 
+        </div>
+      </div>      
+        <div className="video__description">
+          <p>{this.props.mainVideo.description}</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default VideoInfo
